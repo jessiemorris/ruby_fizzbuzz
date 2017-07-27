1055e82bca72c4355889d230ae759a0f8505986f
Ghost <jessiecodes@gmail.com>
2017-07-27 19:19:52 -0400

    who needs parens

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,11 @@
 class Fizzbuzz
   def fizz input
     return "fizzbuzz" if (divisible?(input,3) && divisible?(input,5))
-    return "buzz" if divisible?(input,5)
-    return "fizz" if divisible?(input,3)
+    return "buzz" if divisible? input, 5
+    return "fizz" if divisible? input, 3
     input.to_s
   end
-  def divisible?(top,bottom)
+  def divisible? top, bottom
     top.modulo(bottom).zero?
   end
 end
