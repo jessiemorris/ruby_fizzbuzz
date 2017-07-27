cdefcfc69e5366c2dcdadbc8894feefb6e53933b
Ghost <jessiecodes@gmail.com>
2017-07-27 19:16:26 -0400

    first draft

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,11 @@
 class Fizzbuzz
   def fizz input
-    "fizz"
+    return "fizzbuzz" if (divisible?(input,3) && divisible?(input,5))
+    return "buzz" if divisible?(input,5)
+    return "fizz" if divisible?(input,3)
+    input.to_s
+  end
+  def divisible?(top,bottom)
+    top.modulo(bottom).zero?
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,18 @@
-require 'fizzbuzz.rb'
+require 'fizzbuzz'
 
 describe Fizzbuzz do
   it "returns fizz when passed 3" do
-    expect(Fizzbuzz.new.fizz(3)).to eq("fizz")
+    expect(subject.fizz(3)).to eq("fizz")
+  end
+
+  it "returns 1 when passed 1" do
+    expect(subject.fizz(1)).to eq("1")
+  end
+
+  it "returns buzz when passed 5" do
+    expect(subject.fizz(5)).to eq("buzz")
+  end
+  it "returns fizzbuzz when passed 15" do
+    expect(subject.fizz(15)).to eq("fizzbuzz")
   end
 end
